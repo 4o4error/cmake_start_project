a2a8fe165dfb90dd2f02cca3155ac55f503b301c
4o4error <krullion@gmail.com>
2018-04-18 16:55:11 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,34 +5,5 @@
 void main() {
   
   printHelloWorld();
-  const int size = 15;
-  int test[size];
-
-  for (int i = 0; i < 15; ++i)
-  {
-    test[i] = i;
-  }
-  for (int i = 0; i < 20; ++i)
-  {
-    printf("%d",i);
-  }
   
-  char endOfList[] = "*";
-  FILE* fp = fopen("student_list", "w");
-
-  if (fp != NULL)
-  {
-  }
-  char str[100];
-  do
-  {
-    gets_s(str);
-    if (strcmp(endOfList, str) == 0)
-    {
-      break;
-    }
-    printf("adding %s \n", str);
-    fprintf(fp, str);
-
-  } while (true);
 }
